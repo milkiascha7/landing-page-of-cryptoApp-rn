4f25f3808644e814ad8226b276cd2c4cf843cd05
Milkias Bekele <mikehailu504@gmail.com>
2022-04-07 20:22:51 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../styles/Global'
+
+const Button = ({ link, assetUrl }) => {
+  return (
+    <div 
+        className={styles.btnBlack}
+        onClick={() => window.open(link, "_blank")}
+    >
+        <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
+        <div className='flex flex-col justify-start ml-4'>
+            <p className={`${styles.btnText} font-normal text-xs `}>View it on</p>
+            <p className={`${styles.btnText} font-bold text-sm `}>Expo store</p>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import SectionWrapper from "./SectionWrapper";
+import Features from "./Features";
+import Download from "./Download";
+import Button from "./Button";
+
+
+
+export { SectionWrapper, Features, Download, Button }
